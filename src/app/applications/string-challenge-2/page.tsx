a783e7e7e6366795097eba891993b6218f2c93e4
Mertcan Engin <mertcan.engin@puzzletravel.com>
2025-03-06 10:17:51 +0300

    fix(metadata): BSYS-2381 update siteName
    changed siteName to "toplantim-nerede" in metadata

--- src/app/applications/string-challenge-2/page.tsx
+++ src/app/applications/string-challenge-2/page.tsx
@@ -11,7 +11,7 @@ export const metadata = {
     description: "An application that validates nested HTML tags and identifies mismatches.",
     locale: "en_US",
     type: "website",
-    siteName: "String Challenge",
+    siteName: "toplantim-nerede",
   },
 };
 
